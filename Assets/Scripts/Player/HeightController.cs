f59aedbdcb3d1600f6c95fdc60bb2fc22794729b
huang-feiyu <0711feiyu@gmail.com>
2023-08-04 17:56:07 +0800

    feat: add Slider for changing heights

---
+++ Assets/Scripts/Player/HeightController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+/**
+ * === HeightController.cs ===
+ * Description: C# script that will be attached to HeightController with few children components
+ * Function:
+ *      - Display height value
+ *      - 
+ */
+public class HeightController : MonoBehaviour
+{
+    private const int MaxHeight = 500;
+    public Slider slider;
+    public TMP_Text heightText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0;
+        heightText.text = "0";
+        slider.onValueChanged.AddListener(OnHeightChange);
+    }
+
+    void OnHeightChange(float value)
+    {
+        heightText.text = ((int)(value * MaxHeight)).ToString();
+    }
+}
