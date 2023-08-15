9d828acc269689f3b952a87f3ce8d3f1a9bbc06a
Ludwig Huang <0711feiyu@gmail.com>
2023-08-15 22:16:28 +0800

    feat: add InputField & update fonts

--- Assets/Scripts/Player/HeightController.cs
+++ Assets/Scripts/Player/HeightController.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,19 +15,43 @@ namespace Player
     public class HeightController : MonoBehaviour
     {
         public Slider slider;
-        public TMP_Text heightText;
+        public TMP_InputField heightInput;
 
         void Start()
         {
             slider.value = 0;
-            heightText.text = "0";
+            heightInput.text = "0";
             slider.onValueChanged.AddListener(OnHeightChange);
+            heightInput.onValueChanged.AddListener(OnHeightChange);
+        }
+
+        private void OnHeightChange(string value)
+        {
+            var height = Int32.Parse(value);
+            if (height >= 150)
+            {
+                heightInput.text = "150";
+                height = 150;
+            }
+            else if (height <= 0)
+            {
+                heightInput.text = "0";
+                height = 0;
+            }
+
+            for (var i = 0; i < GlobalManager.Pins.Count; i++)
+            {
+                GlobalManager.Walls[i].selectedCubes = GlobalManager.Pins[i].selectedPins;
+
+                GlobalManager.Pins[i].SetHeight(height);
+                GlobalManager.Walls[i].SetHeight(height);
+            }
         }
 
         void OnHeightChange(float value)
         {
             var height = Mathf.RoundToInt(value * GlobalManager.MaxHeight);
-            heightText.text = height.ToString();
+            heightInput.text = height.ToString();
             for (var i = 0; i < GlobalManager.Pins.Count; i++)
             {
                 GlobalManager.Walls[i].selectedCubes = GlobalManager.Pins[i].selectedPins;
